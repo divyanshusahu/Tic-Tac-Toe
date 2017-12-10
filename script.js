82b9282c565e0c33a5fc946f9cc60b0121bd322c
DivyanshuSahu <dsahu1997@gmail.com>
2017-12-10 19:08:20 +0530

    basic ai created

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var winCombinations = [
 	[2,4,6]
 ];
 
+var gameHasWinner = false;
 const blocks = document.querySelectorAll(".block");
 
 playAgain();
@@ -20,6 +21,7 @@ function playAgain() {
 	for (var i=0;i<9;i++)
 		originalboard[i] = i;
 	document.querySelector(".resultMessage").style.display = "none";
+	gameHasWinner = false;
 	for (var i=0;i<9;i++)
 	{
 		blocks[i].innerText = "";
@@ -30,7 +32,12 @@ function playAgain() {
 
 function turnClick(s) {
 	//console.log(s.target.id);
-	turn(s.target.id, humanPlayer);
+	if (typeof originalboard[s.target.id] == 'number')
+	{
+		turn(s.target.id, humanPlayer);
+		if(!isTie() && gameHasWinner == false)
+			turn(bestSpot(), compPlayer);
+	}
 }
 
 function turn(s, player) {
@@ -38,7 +45,11 @@ function turn(s, player) {
 	document.getElementById(s).innerText = player;
 	let gameWon = checkWin(originalboard, player);
 	if (gameWon)
-		gameOver();
+	{
+		gameHasWinner = true;
+		gameOver(gameWon);
+	}
+	//console.log(gameHasWinner);
 }
 
 function checkWin(board, player) {
@@ -59,7 +70,8 @@ function checkWin(board, player) {
 		//console.log(check);
 		if (isEqual(check,winCombinations[i]))
 		{
-			gameWon = true;
+			gameWon = {index:i, player:player};
+			//console.log(gameWon);
 			break;
 		}
 	}
@@ -90,4 +102,37 @@ function isEqual (a,b) {
 			return false;
 	}
 	return true;
+}
+
+function gameOver(a) {
+	//console.log(a);
+	//console.log(a['index']);
+	let winIndex = a['index'];
+	let winner = a['player'];
+	//console.log(winner == humanPlayer);
+	for (var i=0;i<3;i++)
+		blocks[winCombinations[winIndex][i]].style.backgroundColor = (winner == humanPlayer ? "blue" : "red");
+	for (var i=0;i<9;i++)
+		blocks[i].removeEventListener("click", turnClick);
+}
+
+function bestSpot() {
+	return emptyBlocks()[0];
+}
+
+function emptyBlocks() {
+	return originalboard.filter(n => typeof n == 'number');
+}
+
+function isTie() {
+	if (emptyBlocks().length == 0 && gameHasWinner == false)
+	{
+		for (var i=0;i<9;i++)
+		{
+			blocks[i].style.backgroundColor = "green";
+			blocks[i].removeEventListener("click", turnClick);
+		}
+		return true;
+	}
+	return false;
 }
